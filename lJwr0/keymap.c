60a3b695c13eb35ffb9604cb0abb75d23a850901
Brock <brock.perreca@gmail.com>
2026-01-18 15:28:09 -0700

    Update keymap.c

--- lJwr0/keymap.c
+++ lJwr0/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,        
     KC_BSPC,        LT(3, KC_A),    MT(MOD_LCTL, KC_S),MT(MOD_LSFT, KC_D),LT(2, KC_F),    KC_G,                                           KC_H,           LT(2, KC_J),    MT(MOD_RSFT, KC_K),MT(MOD_RCTL, KC_L),LT(3, KC_SCLN), KC_QUOTE,       
     CW_TOGG,        MT(MOD_LGUI, KC_Z),MT(MOD_LALT, KC_X),KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       MT(MOD_RALT, KC_DOT),KC_SLASH,       KC_MEH,         
-                                                    LT(3, KC_SPACE),KC_LEFT_CTRL,                                   TT(1),          LT(2, KC_ENTER)
+                                                    LT(3, KC_SPACE),QK_REP,                                         TT(1),          LT(2, KC_ENTER)
   ),
   [1] = LAYOUT_voyager(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, QK_LLCK,        
